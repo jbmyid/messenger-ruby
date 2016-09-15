ef6de3b49931b522e0b6f685812fb656bcce7245
jbmyid <jbmyid@gmail.com>
2016-09-15 12:07:06 -0700

    Update configuration.rb

--- lib/messenger/configuration.rb
+++ lib/messenger/configuration.rb
@@ -2,7 +2,7 @@ module Messenger
   class Configuration
     attr_accessor :verify_token, :page_access_token
 
-    def initialize(verify_token: nil, page_access_token: nil)
+    def initialize(verify_token= nil, page_access_token= nil)
       @verify_token = verify_token
       @page_access_token = page_access_token
     end
